df0664821a4aa19586dfdee2cacc35620f26d850
软盘君 <68799904+ifloppy@users.noreply.github.com>
2020-11-01 15:17:40 +0800

    Add files via upload

--- proxy.php
+++ proxy.php
@@ -1,9 +1,8 @@
 <?php
+if(isset($_GET['url'])==false){die("请将参数填写完整，详见https://github.com/ifloppy/phpSimpleProxy");}
 $token=(string)rand(1000,9999);
-$file=fopen($token, "w");
 $data_down=file_get_contents($_GET['url']);
-fwrite($file, $data_down);
-fclose($file);
+file_put_contents($token, $data_down);
 header('Content-Type: '.mime_content_type($token));
 unlink($token);
 echo $data_down;
