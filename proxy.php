030b85ff13a7fde089353a1a21983c1156b68f5f
软盘君 <68799904+ifloppy@users.noreply.github.com>
2020-11-01 14:31:29 +0800

    update
    添加MIME类型检测

--- proxy.php
+++ proxy.php
@@ -1,3 +1,11 @@
 <?php
-echo file_get_contents($_GET['url']);
+$token=(string)rand(1000,9999);
+$file=fopen($token, "w");
+$data_down=file_get_contents($_GET['url']);
+fwrite($file, $data_down);
+$bin = fread($file, 2);
+fclose($file);
+header('Content-Type: '.mime_content_type($token));
+unlink($token);
+echo $data_down;
 ?>
