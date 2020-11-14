6c9c5577da899b04c77f0f36e9e2ff68f351a315
软盘君 <68799904+ifloppy@users.noreply.github.com>
2020-11-14 21:23:50 +0800

    将get_file_contents替换为curl

--- proxy.php
+++ proxy.php
@@ -1,7 +1,18 @@
 <?php
 if(isset($_GET['url'])==false){die("请将参数填写完整，详见https://github.com/ifloppy/phpSimpleProxy");}
 $token=(string)rand(1000,9999);
-$data_down=file_get_contents($_GET['url']);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $_GET['url']);
+curl_setopt ($ch,  CURLOPT_HEADER,false);
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION ,1); 
+if(isset($_SERVER['HTTP_REFERER'])==true){curl_setopt($ch, CURLOPT_REFERER, $_SERVER['HTTP_REFERER']);}
+$data_down = curl_exec($ch);
+if($data_down  === FALSE){die("代理时发生错误");}
+curl_close($ch);
 file_put_contents($token, $data_down);
 header('Content-Type: '.mime_content_type($token));
 unlink($token);
